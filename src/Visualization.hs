f276279461e67d205783d206e85d6f7631eb60cd
Daniel Lyons <fusion@storytotell.org>
2012-04-09 16:58:04 -0600

    switching to gchart package

--- src/Visualization.hs
+++ src/Visualization.hs
@@ -3,21 +3,21 @@ module Visualization where
 import Data.List
 import Data.Maybe
 
+import Graphics.GChart
+import Graphics.GChart.Types
 import Network.URI
 
 import Types
 
-percentWeightLoss :: [(Name, Double)] -> URI
+percentWeightLoss :: [(Name, Float)] -> URI
 percentWeightLoss dat =
-    fromJust $ parseURI $
-      concat [ base
-             , "?chs=400x350&cht=p&chco=224499&chds=-10,100&chd=t:"
-             , dataPoints 
-             , "&chdl=" 
-             , dataLabels
-             , "&chtt=Weight+Loss" ]
-  where
-    base = "http://chart.apis.google.com/chart"
-    dataPoints = escape $ intercalate "," $ map (show . snd) dat
-    dataLabels = escape $ intercalate "|" $ map fst dat
-    escape str = escapeURIString isUnreserved str
+    fromJust $ parseURI $ escape $ getChartUrl $ do
+      setChartSize 400 350
+      setChartType Pie
+      setChartTitle "Weight Loss"
+      setDataEncoding text
+      addChartData $ map snd dat
+      addColor "224499"
+      setLabels $ map fst dat
+    where
+      escape str = escapeURIString (/='|') str
